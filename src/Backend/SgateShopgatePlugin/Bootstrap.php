bc840da773cb67eff4a321f3a74ef925b4b96cd2
Carsten Schneider <carsten.schneider@shopgate.com>
2020-06-29 13:42:32 +0200

    set template vars for bonus pages

--- src/Backend/SgateShopgatePlugin/Bootstrap.php
+++ src/Backend/SgateShopgatePlugin/Bootstrap.php
@@ -428,6 +428,12 @@ class Shopware_Plugins_Backend_SgateShopgatePlugin_Bootstrap extends Shopware_Co
             );
 
             $this->subscribeEvent(
+              'Enlight_Controller_Action_Frontend_BonusSystem',
+              // or 'Enlight_Controller_Action_PostDispatch_Frontend_BonusSystem' ?
+              'onFrontendCustom'
+            );
+
+            $this->subscribeEvent(
                 'Enlight_Controller_Action_Frontend_Account_Password',
                 'onFrontendPassword'
             );
