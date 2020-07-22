f4ff5ebcbf38e1e18d05d4a8d9e22e7028dbe4a3
André Kraus <andre.kraus@shopgate.com>
2020-07-22 12:10:04 +0200

    prepared 2.9.91-rc.1

--- src/Backend/SgateShopgatePlugin/Bootstrap.php
+++ src/Backend/SgateShopgatePlugin/Bootstrap.php
@@ -155,7 +155,7 @@ class Shopware_Plugins_Backend_SgateShopgatePlugin_Bootstrap extends Shopware_Co
      */
     public function getVersion()
     {
-        return '2.9.91-test.1';
+        return '2.9.91-rc.1';
     }
 
     /**
